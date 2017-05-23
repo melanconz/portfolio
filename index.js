bc61a169d7090d226605306fc2dde26ea63fabcf
Zachary Melancon <zmelan01@gmail.com>
2017-05-22 22:44:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+  response.render('index')
+  response.send('Hello from Express!')
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
